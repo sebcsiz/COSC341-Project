b214413179a6438c78981bc6bed669b623e1db5e
JanineFM <jmatter@student.ubc.ca>
2025-11-30 09:26:31 -0800

    changed MissionHill to WineryInfo

--- app/src/main/java/com/example/cosc341project/WineryInfoActivity.java
+++ app/src/main/java/com/example/cosc341project/WineryInfoActivity.java
@@ -8,12 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MissionHillActivity extends AppCompatActivity {
+public class WineryInfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_mission_hill);
+        setContentView(R.layout.activity_winery_info);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
